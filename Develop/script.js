3f5f6193b1f4507fd92b3976fcf40e473a35eb01
rtrevill <139879271+rtrevill@users.noreply.github.com>
2023-09-23 20:01:38 +1000

    centred text, formatted button including hover

--- Develop/script.js
+++ Develop/script.js
@@ -121,6 +121,14 @@ location.reload();
 
 
 });
+
+
+$('#clrButton').hover(function(){
+  $(this).css('background-color', 'red');
+},
+function(){
+  $(this).css('background-color', '#ff6961');
+});
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
